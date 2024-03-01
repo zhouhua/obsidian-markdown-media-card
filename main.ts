997ac47cb38c393b8e3a220408610bda2b29d24c
Zhou Hua <zhou--hua@163.com>
2024-03-01 19:25:21 +0800

    处理异常情况

--- main.ts
+++ main.ts
@@ -1,10 +1,28 @@
-import { Plugin, parseYaml } from "obsidian";
+import {
+	Plugin,
+	parseYaml,
+	MarkdownRenderer,
+	MarkdownRenderChild,
+	Notice,
+} from "obsidian";
 import { template } from "@zhouhua-dev/remark-media-card";
 
 export default class MediaCardPlugin extends Plugin {
 	async onload() {
-		this.registerMarkdownCodeBlockProcessor("media-card", (source, el) => {
-			el.innerHTML = template(parseYaml(source));
+		this.registerMarkdownCodeBlockProcessor("media-card", (source, el, ctx) => {
+			console.log("render");
+			try {
+				el.innerHTML = template(parseYaml(source));
+			} catch (e) {
+				new Notice("Invalid YAML format.");
+				MarkdownRenderer.render(
+					this.app,
+					["```yaml", source, "```"].join("\n"),
+					el,
+					ctx.sourcePath,
+					new MarkdownRenderChild(el)
+				);
+			}
 		});
 	}
 
