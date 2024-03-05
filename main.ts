3d60d48759bcc3328f826fcd6363dd9aeb175328
Zhou Hua <zhou--hua@163.com>
2024-03-05 20:44:38 +0800

    fix some issues from CR

--- main.ts
+++ main.ts
@@ -4,19 +4,28 @@ import {
 	MarkdownRenderer,
 	MarkdownRenderChild,
 	sanitizeHTMLToDom,
-	Notice,
 } from "obsidian";
 import { template } from "@zhouhua-dev/remark-media-card";
+
+function appendErrorMsg(el: HTMLElement) {
+	const container = el.querySelector("pre.language-yaml");
+	if (container) {
+		const errorMsg = container.createEl("div", {
+			cls: "error-msg",
+			text: "Invalid YAML format.",
+		});
+	}
+}
 
 export default class MediaCardPlugin extends Plugin {
 	async onload() {
 		this.registerMarkdownCodeBlockProcessor("media-card", (source, el, ctx) => {
+			el.addClass("markdown-media-card-render");
 			try {
 				const html = template(parseYaml(source));
 				const fragment = sanitizeHTMLToDom(html);
 				el.appendChild(fragment);
 			} catch (e) {
-				new Notice("Invalid YAML format.");
 				MarkdownRenderer.render(
 					this.app,
 					["```yaml", source, "```"].join("\n"),
@@ -24,6 +33,7 @@ export default class MediaCardPlugin extends Plugin {
 					ctx.sourcePath,
 					new MarkdownRenderChild(el)
 				);
+				appendErrorMsg(el);
 			}
 		});
 	}
